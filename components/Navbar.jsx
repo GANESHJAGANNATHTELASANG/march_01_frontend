9140f16c99aff316fd207645ea58b152a7ae3008
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-05-25 21:07:08 +0530

    fixed dotenv and port

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = ({ setQuery }) => {
   const { user, logout } = useAuth();
 
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 flex items-center justify-between">
+    <nav className="bg-gray-900 text-white px-4 py-4 flex flex-col md:flex-row items-center justify-between gap-4">
       {/* Logo */}
       <div className="text-2xl font-bold text-blue-400">Note_App</div>
 
       {/* Search Bar */}
-      <div className="bg-white shadow-md px-6 py-4 flex items-center justify-between">
+      <div className="bg-white shadow-md px-4 py-3 rounded-lg w-full md:w-[400px]">
         <input
           type="text"
           onChange={(e) => setQuery(e.target.value)}
@@ -21,26 +21,27 @@ const Navbar = ({ setQuery }) => {
       </div>
 
       {/* Buttons */}
-
-      <div className="flex gap-4">
+      <div className="flex flex-wrap justify-center gap-4">
         {!user ? (
           <>
             <button
               type="button"
               className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded-lg"
             >
-              <Link to="/login"> Login </Link>
+              <Link to="/login">Login</Link>
             </button>
+
             <button
               type="button"
               className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-lg"
             >
-              <Link to="/register"> Sign up </Link>
+              <Link to="/register">Sign up</Link>
             </button>
           </>
         ) : (
           <>
-            <button className="  px-4 py-2 rounded-lg">{user.name}</button>
+            <button className="px-4 py-2 rounded-lg">{user.name}</button>
+
             <button
               onClick={logout}
               className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded-lg"
